b65b7d94717af02102cde2c3428ffb6884bad201
artik3314@gmail.com <artik3314@gmail.com>
2022-12-09 20:43:57 +0300

    Тестирование модели представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CV19.ViewModels.BaseVIewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace CV19.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+
+
+        // дизайнер WPF работает с бинарниками, поэтому каждый раз
+        // как что-то поменяли в коде надо пересобрать проект
+
+        /// <summary>
+        /// Заголовок окна 
+        /// </summary>
+        #region Title
+        private string _Title;
+
+        public string Title
+        {
+            get => _Title;
+
+            /*
+            set
+            {
+                if(Equals(_Title,value)) return;
+                _Title = value;
+                OnPropertyChanged();
+
+                Set(ref _Title, value);
+            }
+            */
+
+
+            set => Set(ref _Title, value);
+        } 
+        #endregion
+
     }
 }
