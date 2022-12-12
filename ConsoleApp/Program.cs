002875830f7efa99fb82639132b133578bc49a4c
artik3314@gmail.com <artik3314@gmail.com>
2022-12-12 22:54:43 +0300

    Проверка логики

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -29,7 +29,7 @@ namespace ConsoleApp
 
                 if (string.IsNullOrEmpty(line)) continue;
 
-                yield return line;
+                yield return line.Replace("Korea,","Korea -");
 
             }
 
@@ -43,7 +43,22 @@ namespace ConsoleApp
             .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
 
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
 
+            foreach(var row in lines)
+            {
+                var province = row[0].Trim();
+                var country_name = row[1].Trim(' ','"');
+                var counts = row.Skip(5)
+                    .Select(int.Parse).ToArray();
+
+                yield return (country_name, province,counts);
+            }
+        }
 
 
 
@@ -52,7 +67,8 @@ namespace ConsoleApp
 
         static void Main(string[] args)
         {
-            //var client = new HttpClient(); 
+
+            //var client = new HttpClient();
 
             //var respose = client.GetAsync(data_url).Result;
 
@@ -61,9 +77,14 @@ namespace ConsoleApp
             //Console.ReadLine();
 
 
-            var dates = GetDates();
-            Console.WriteLine(string.Join("\r\n", dates));
+            //var dates = GetDates();
+            //Console.WriteLine(string.Join("\r\n", dates));
 
+            var russia_data = GetData().First(v => v.Country.Equals("Russia", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia_data.Counts, (date,count) => $"{date:dd:MM:yy} - {count}" )));
+
+            Console.ReadLine();
         }
     }
 }
